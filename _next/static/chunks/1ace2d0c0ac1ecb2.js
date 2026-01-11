fc86b126aec57e24fad937098321feac13c94d5d
AVAVT <itsavavt@gmail.com>
2026-01-11 13:38:50 +0700

    Updates

--- _next/static/chunks/1ace2d0c0ac1ecb2.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/48692320b37ddf6a.js",
-  "static/chunks/turbopack-5b418cf622e610ec.js"
-])
--- _next/static/chunks/b2f72795f43e8ab5.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/48692320b37ddf6a.js",
-  "static/chunks/turbopack-99a99c764ee8116b.js"
-])
